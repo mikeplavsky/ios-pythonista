6a0bfb022ea726aded3dc3b99a3004990e0889dd
Mikhail Plavskiy <mikhail.plavskiy@quest.com>
2018-09-30 00:30:29 +0300

    getting stories for current sprint

--- jira.py
+++ jira.py
@@ -4,12 +4,27 @@ from requests.auth import HTTPBasicAuth
 from save import get_credentials
 from config import jira_host
 
-def get_curr_sprint(board_id):
-	
-	jira_url = f'https://{jira_host}/rest/agile/latest/board/{board_id}/sprint?state=active'
+def get_jira(url):
 	
 	usr, pwd = get_credentials('jira')
 	
-	res = r.get(jira_url, 
+	res = r.get(url, 
 		auth=HTTPBasicAuth(usr,pwd))
-	return res 
+		
+	res.raise_for_status()
+		
+	return res.json()
+
+def get_curr_sprint(board_id):
+	
+	url = f'https://{jira_host}/rest/agile/latest/board/{board_id}/sprint?state=active'
+	
+	return get_jira(url)['values'][0]
+	
+def get_curr_stories(board_id):
+	
+	sprint = get_curr_sprint(board_id)['id']
+	
+	url = f'https://{jira_host}/rest/agile/latest/board/{board_id}/sprint/{sprint}/issue'
+	
+	return get_jira(url)
