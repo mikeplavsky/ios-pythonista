b7057d5444e96b1b05df90fabbbbbc8ffc056fc2
mikeplavsky <mike.plavsky@quest.com>
2019-02-27 12:25:21 +0300

    spaces to tabs and url for stories

--- jira.py
+++ jira.py
@@ -11,100 +11,100 @@ import clipboard
 import appex
 
 def log(str):
-	if appex.is_running_extension():
-		return
-	print(str)
-	
+    if appex.is_running_extension():
+        return
+    print(str)
+    
 def request_jira(method,url,json=None):
-	
-	usr, pwd = get_credentials('jira')
-	
-	res = method(
-		url, 
-		auth=HTTPBasicAuth(usr,pwd),
-		json=json)
-		
-	log(f'{method.__name__} {url} ...')
-	res.raise_for_status()
-	log('done.')
-	
-	return res
+    
+    usr, pwd = get_credentials('jira')
+    
+    res = method(
+        url, 
+        auth=HTTPBasicAuth(usr,pwd),
+        json=json)
+        
+    log(f'{method.__name__} {url} ...')
+    res.raise_for_status()
+    log('done.')
+    
+    return res
 
 def get_jira(url):
-	return request_jira(r.get,url).json()
+    return request_jira(r.get,url).json()
 
 def get_curr_sprint(board_id):
-	
-	url = f'https://{jira_host}/rest/agile/latest/board/{board_id}/sprint?state=active'
-	return get_jira(url)['values'][0]
-	
+    
+    url = f'https://{jira_host}/rest/agile/latest/board/{board_id}/sprint?state=active'
+    return get_jira(url)['values'][0]
+    
 def get_curr_stories(board_id):
-	
-	sprint = get_curr_sprint(board_id)['id']
-	url = f'https://{jira_host}/rest/agile/latest/board/{board_id}/sprint/{sprint}/issue'
-	
-	return get_jira(url)
-	
+    
+    sprint = get_curr_sprint(board_id)['id']
+    url = f'https://{jira_host}/rest/agile/latest/board/{board_id}/sprint/{sprint}/issue'
+    
+    return get_jira(url)
+    
 def enum_stories(board_id, s=''):
-	
-	issues = get_curr_stories(board_id)['issues']
-	
-	subtask = lambda x: x['fields']['issuetype']['subtask']
-	status = lambda x: x['fields']['status']['name']
-	in_status = lambda x: True if not s else status(x) == s
+    
+    issues = get_curr_stories(board_id)['issues']
+    
+    subtask = lambda x: x['fields']['issuetype']['subtask']
+    status = lambda x: x['fields']['status']['name']
+    in_status = lambda x: True if not s else status(x) == s
 
-	all = [f"{v['fields']['summary']}" for _,v in enumerate(issues) if not subtask(v) and in_status(v)]
-	
-	res = '\n\n'.join(all)
-	print(res)
-	
-	clipboard.set(res)
-	
-	
+    all = [f"{v['fields']['summary']}\nhttps://{jira_host}/browse/{v['key']}" for _,v in enumerate(issues) if not subtask(v) and in_status(v)]
+    
+    res = '\n\n'.join(all)
+    print(res)
+    
+    clipboard.set(res)
+    
+    
 def get_versions(project):
-	
-	url = f'https://{jira_host}/rest/api/latest/project/{project}/version'
+    
+    url = f'https://{jira_host}/rest/api/latest/project/{project}/version'
 
-	res = request_jira(r.get, url).json()
-	[print(v['name']) for v in res['values']]
-	
+    res = request_jira(r.get, url).json()
+    [print(v['name']) for v in res['values']]
+    
 def delete_issue(key):
-	
-	url = f'https://{jira_host}/rest/api/latest/issue/{key}'
-	return request_jira(r.delete, url)
-	
+    
+    url = f'https://{jira_host}/rest/api/latest/issue/{key}'
+    return request_jira(r.delete, url)
+    
 def get_issue(key):
-	
-	url = f'https://{jira_host}/rest/api/latest/issue/{key}'
+    
+    url = f'https://{jira_host}/rest/api/latest/issue/{key}'
 
-	res = request_jira(r.get, url).json()
-	fs = res['fields']
-	
-	print(
-		f"\n{fs['summary']}\n\n{fs['description']}")
-		
-	return res
+    res = request_jira(r.get, url).json()
+    fs = res['fields']
+    
+    print(
+        f"\n{fs['summary']}\n\n{fs['description']}")
+        
+    return res
 
 def create_issue(
-	project,
-	summary,
-	description=''):
-	
-	issue = dict(
-		fields=dict(
-			project=dict(key=f'{project}'),
-			summary=f'{summary}',
-			description=f'{description}',
-			issuetype=dict(name='Story')))
-	
-	url = f'https://{jira_host}/rest/api/latest/issue/'
-	
-	res = request_jira(
-		r.post, url, issue).json()
-	key = res['key']
-	
-	return f'https://{jira_host}/browse/{key}'
-	
-	
-		
-	
+    project,
+    summary,
+    description=''):
+    
+    issue = dict(
+        fields=dict(
+            project=dict(key=f'{project}'),
+            summary=f'{summary}',
+            description=f'{description}',
+            issuetype=dict(name='Story')))
+    
+    url = f'https://{jira_host}/rest/api/latest/issue/'
+    
+    res = request_jira(
+        r.post, url, issue).json()
+    key = res['key']
+    
+    return f'https://{jira_host}/browse/{key}'
+    
+    
+        
+    
