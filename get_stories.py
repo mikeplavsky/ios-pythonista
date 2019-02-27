29df5f9bd0df348744464d62caf6ec23eb05a24d
mikeplavsky <mike.plavsky@quest.com>
2019-02-28 00:40:29 +0300

    creating a note

--- get_stories.py
+++ get_stories.py
@@ -7,15 +7,17 @@ import json
 from jira import enum_stories
 
 def main():
-	
-	project = json.loads(sys.argv[1])
-	console.clear()
-	
-	print(project)
-	
-	enum_stories(
-		project['project'],
-		project['status'])
+    
+    project = json.loads(sys.argv[1])
+    console.clear()
+    
+    print(project)
+    
+    enum_stories(
+        project['project'],
+        project['status'])
+    
+    webbrowser.open_new('shortcuts://run-shortcut?name=CreateANote')
 
 if __name__ == '__main__':
-	main()
+    main()
