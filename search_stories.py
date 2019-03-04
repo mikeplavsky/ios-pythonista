6865fc58ad21264d6f032a43aa2c77c83d905978
mikeplavsky <mike.plavsky@quest.com>
2019-03-04 12:16:14 +0300

    searching for stories

---
+++ search_stories.py
@@ -0,0 +1,30 @@
+import clipboard
+import console
+import sys
+import webbrowser
+import json
+
+from jira import search_for_stories
+
+import jira
+from save import get_credentials
+jira.get_credentials = get_credentials
+
+def main():
+    
+    project = json.loads(sys.argv[1])
+    console.clear()
+    
+    print(project)
+    
+    all = search_for_stories(
+        project['project'],
+        project['query'])
+
+    res = '\n\n'.join(all)
+
+    clipboard.set(res)
+    webbrowser.open_new('shortcuts://run-shortcut?name=CreateANote')
+
+if __name__ == '__main__':
+    main()
