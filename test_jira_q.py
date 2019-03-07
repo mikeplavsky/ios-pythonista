4cb982f3b7381eb50be4d61802be6e56f81d4caa
mikeplavsky <mike.plavsky@quest.com>
2019-03-07 12:17:16 +0300

    covered versions

--- test_jira_q.py
+++ test_jira_q.py
@@ -1,16 +1,28 @@
 import jira
 import pytest
 
-@pytest.mark.parametrize("points, res",[
+@pytest.mark.parametrize("points, exp",[
     (10, "10 points"),
     (0, "0 points"),
     ("", ""),
     (1,"1 point")])
-def test_points(points, res):
+def test_points(points, exp):
 
     given = dict(
         fields=dict(
             customfield_10303 = f"{points}"))
             
     then = jira.story_points(given)
-    assert then == res 
+    assert then == exp
+
+@pytest.mark.parametrize("versions, exp",[
+    ([],"no releases"),
+    ([dict(name="1"), dict(name="2")],"1,2")])
+def test_versions(versions, exp):
+
+    given = dict(
+        fields = dict(
+            fixVersions = versions))
+
+    then = jira.versions(given)         
+    assert then == exp 
