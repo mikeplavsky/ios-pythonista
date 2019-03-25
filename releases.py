a895411c483a50ec8d08c7cc62992ea6f54927bf
mikeplavsky <mike.plavsky@quest.com>
2019-03-26 00:49:11 +0300

    added title

--- releases.py
+++ releases.py
@@ -15,20 +15,20 @@ vs = jira.get_versions_names(
     dict(project= project))
 
 version = list_dialog(items=vs)
-
 epics = jira.get_epics(project, version)
-epics.append("X")
 
 while True:
 
     epic = list_dialog(items=epics)
-    if epic == 'X':
+    if not epic:
         break
 
     issues = jira.get_epic_issues(project,version,epic)
     all = jira.fmt_issues(issues)
 
-    all.insert(0,f"Epic: {epic}")
+    all.insert(
+        0,
+        f"{project}, {version}, {epic}")
     res = '\n\n'.join(all)
 
     clipboard.set(res)
