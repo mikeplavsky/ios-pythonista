879ae691456e72b56aaf5271afabbf3f281972e9
mikeplavsky <mike.plavsky@quest.com>
2019-03-26 00:18:46 +0300

    UI tweaks

--- releases.py
+++ releases.py
@@ -13,23 +13,25 @@ project = list_dialog(
         
 vs = jira.get_versions_names(
     dict(project= project))
+
 version = list_dialog(items=vs)
 
 epics = jira.get_epics(project, version)
-epics.append("Done")
+epics.append("X")
 
 while True:
 
     epic = list_dialog(items=epics)
-    if epic == 'Done':
+    if epic == 'X':
         break
 
     issues = jira.get_epic_issues(project,version,epic)
     all = jira.fmt_issues(issues)
 
-    url = f'shortcuts://run-shortcut?name=CreateANote'
+    all.insert(0,f"Epic: {epic}")
     res = '\n\n'.join(all)
 
     clipboard.set(res)
-    webbrowser.open_new(url)
+    webbrowser.open_new(
+        'shortcuts://run-shortcut?name=CreateANote')
 
