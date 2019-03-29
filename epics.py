91cd700526ec97f512fb51f6b90b693bc4cf2b40
mikeplavsky <mike.plavsky@quest.com>
2019-03-30 01:24:05 +0300

    colors research

--- epics.py
+++ epics.py
@@ -10,7 +10,9 @@ class ReleaseDelegate(object):
         nav.push_view(epics)
 
 releases = ui.TableView()
-ds = ui.ListDataSource([1,2,3])
+
+ds = ui.ListDataSource([1,2,3,4])
+ds.highlight_color = (0.7,0.7,0.7,0.5)
 
 releases.data_source = ds 
 releases.delegate = ReleaseDelegate()
