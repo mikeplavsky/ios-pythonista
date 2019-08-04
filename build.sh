c4d447385f5cbb81aff83a4065d2c1e8d88e6283
mikplavsky <mikhail.plavskiy@quest.com>
2019-08-05 01:00:51 +0300

    better build

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
+if [ -z $1 ]; then
+    echo "version is required"
+    exit 1
+fi
+
+VERSION=$1
+
 docker build -t jira-proxy .
+docker tag jira-proxy mikeplavsky/jira-proxy:${VERSION}
+docker push mikeplavsky/jira-proxy:${VERSION}
